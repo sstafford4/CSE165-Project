241dd734542f15d4781d129c560db132d3bac913
Spencer Stafford <152045915+sstafford4@users.noreply.github.com>
2024-04-21 16:11:48 -0700

    Update npc.h
    updated to function with collide() and added accessors

--- npc.h
+++ npc.h
@@ -6,20 +6,19 @@
 
 using namespace std;
 
-// In the event that i cant get the npc moving back and forth, then I can do
-// a bullet bill type enemy, where they move out of the stage and respawn.
-
 class NPC {
- 
-	float square_pos_x = 0.0f; //initial position of square
+protected:
+	float square_pos_x = 0.6f; //initial position of square
 	float sq_speed_x = 0.00005f; //speed of movement of the square
 	// ideal speed is 0.00005f
-
-	// variables for vertical movement
 	float square_pos_y = 0.0f;
-	float sq_speed_y = 0.00005f; // just setting it to the same as x.
+
 public:
-	NPC() {}; 
+	NPC(float initialX, float initialY, float initialSpeed) {
+		square_pos_x = initialX;
+		square_pos_y = initialY;
+		sq_speed_x = initialSpeed; 
+		}; 
 	void display() {
 
 		glBegin(GL_QUADS); 
@@ -37,14 +36,17 @@ public:
 	void side_movement() {
 		// in the window, 1 represents the right barrier, -1 represents the left barrier
 		square_pos_x += sq_speed_x; 
-		if (square_pos_x > 0.8f || square_pos_x < -1.0f) {
+		if (square_pos_x > 0.8f || square_pos_x < -0.4f) {
 			sq_speed_x = -sq_speed_x;
 			square_pos_x += sq_speed_x;
 		}
 	}
-	// will add vertical movement
-	void vert_movement() {
+	float getNpcX() {
+		return square_pos_x;
+	}
 
+	float getNpcY() {
+		return square_pos_y; 
 	}
 
 	~NPC() {}
