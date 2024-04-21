0f1d01de004b48f8e60c15644617bdc349068abf
Spencer Stafford <152045915+sstafford4@users.noreply.github.com>
2024-04-21 12:32:46 -0700

    Update npc.h
    Updated, added default constructor that does nothing.

--- npc.h
+++ npc.h
@@ -2,49 +2,51 @@
 #include<iostream>
 #include<vector>
 #include <GLFW/glfw3.h>
+#include<cmath>
 
 using namespace std;
 
-class NPC {
-	float square_pos = 0.0f; //initial position of square
-	float sq_speed = 0.00005f; //speed of movement of the square
-	
+// In the event that i cant get the npc moving back and forth, then I can do
+// a bullet bill type enemy, where they move out of the stage and respawn.
 
+class NPC {
+ 
+	float square_pos_x = 0.0f; //initial position of square
+	float sq_speed_x = 0.00005f; //speed of movement of the square
+	// ideal speed is 0.00005f
+
+	// variables for vertical movement
+	float square_pos_y = 0.0f;
+	float sq_speed_y = 0.00005f; // just setting it to the same as x.
 public:
+	NPC() {}; 
 	void display() {
-		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBegin(GL_QUADS); 
 		glColor3f(0.0f, 1.0f, 1.0f);// teal color
 		//size of the square
-		glVertex2f(square_pos, 0.0f);
-		glVertex2f(square_pos + 0.2f, 0.0f);
-		glVertex2f(square_pos + 0.2f, 0.2f);
-		glVertex2f(square_pos, 0.2f);
+		glVertex2f(square_pos_x, 0.0f);
+		glVertex2f(square_pos_x + 0.2f, 0.0f);
+		glVertex2f(square_pos_x + 0.2f, 0.2f);
+		glVertex2f(square_pos_x, 0.2f);
 
 		glEnd();
 	}
 
-	// placeholder for a potential second square
-	/*void display2() {
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_QUADS);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(square + 1.0f, 0.0f);
-		glVertex2f(square + 1.2f, 0.0f);
-		glVertex2f(square + 1.2f, 1.2f);
-		glVertex2f(square + 1.0f, 1.2f);
-
-		glEnd();
-	}*/
-
-	// square currently moves to the right and stops at the border of the window
-	void movement() {
-		square_pos += sq_speed;
-		if (square_pos > 0.8f || square_pos < -1.0f) {
-			square_pos -= sq_speed;
-			//square_pos += sq_speed; 
+	// moves side to side
+	void side_movement() {
+		// in the window, 1 represents the right barrier, -1 represents the left barrier
+		square_pos_x += sq_speed_x; 
+		if (square_pos_x > 0.8f || square_pos_x < -1.0f) {
+			sq_speed_x = -sq_speed_x;
+			square_pos_x += sq_speed_x;
 		}
 	}
+	// will add vertical movement
+	void vert_movement() {
+
+	}
+
+	~NPC() {}
+
 };
