588c11bd1ea627cd81211a649b45a41474a4bb17
Spencer Stafford <152045915+sstafford4@users.noreply.github.com>
2024-04-24 15:46:00 -0700

    Update NPCVirt.h
    Updated NPCVirt.h to work with enemy.h

--- NPCVirt.h
+++ NPCVirt.h
@@ -12,8 +12,10 @@ public:
 	NPCVirt(float initialX, float initialY, float initialSpeed)
 		: x(initialX), y(initialY), speed(initialSpeed) {}
 
-	virtual void b_movement() = 0; 
-	virtual void b_display() const = 0;
+	virtual void side_movement() = 0;
+	virtual void scroll_movement() = 0;
+	virtual void display() const = 0;
+	virtual void display(float r, float g, float b) = 0; 
 
-
+	virtual void npc_death() = 0; 
 };
